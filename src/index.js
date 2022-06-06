d8bd3e9b155bf260093c2b690ccbe4f2e527aad7
Maxwell Sayles <maxwellsayles@gmail.com>
2022-06-06 16:32:21 -0700

    Hello World!

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// @flow
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+function HelloWorld() {
+    return <div>Hello World!</div>;
+}
+
+const xx = <HelloWorld />;
+const root = document.getElementById('root');
+if (root === null) {
+  alert('You must specify a <div id="root">');
+} else {
+  ReactDOM.render(xx, root);
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+    inject: 'body',
+});
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'bin'),
+        filename: 'bundle.js'
+    },
+    module: {
+	rules: [
+	    {
+		test: /\.m?js$/,
+		exclude: /(node_modules|bower_components)/,
+		use: {
+		    loader: 'babel-loader',
+		    options: {
+			presets: [
+			    "@babel/preset-flow",
+			    "@babel/preset-env",
+			    "@babel/preset-react",
+			],
+			plugins: [
+			    "@babel/plugin-proposal-class-properties",
+			],
+		    },
+		},
+	    },
+	],
+    },
+    plugins: [HtmlWebpackPluginConfig],
+}
